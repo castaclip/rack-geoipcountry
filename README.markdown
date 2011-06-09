13e17af6b65b80f2e551d05b9bb33f22e18f4bda
Daniel Lohse <daniel.lohse@castaclip.com>
2011-06-09 14:44:18 +0200

    Added remaining information from the geo information lookup.

--- README.markdown
+++ README.markdown
@@ -20,6 +20,9 @@ The full set of GEOIP request headers is below:
 * X_GEOIP_COUNTRY_CODE3 - The ISO3166-2 three-character country code, if not found set to --
 * X_GEOIP_COUNTRY - The ISO3166 English-language name of the country, if not found set to N/A
 * X_GEOIP_CONTINENT - The two-character continent code, if not found set to --
+* X_GEOIP_REGION - The English-language name of the region, if not found set to "" (e.g. "CA")
+* X_GEOIP_CITY - The English-language name of the city, if not found set to "" (e.g. "Mountain View")
+* X_GEOIP_POSTAL_CODE - The postal code, if not found set to "" (e.g. "94043")
 
 You can use the included Mapping class to trigger lookup only for certain requests by specifying matching path prefix in options, eg:
 
--- lib/rack/geoipcountry.rb
+++ lib/rack/geoipcountry.rb
@@ -25,6 +25,10 @@ module Rack
   # X_GEOIP_COUNTRY_CODE3 - The ISO3166-2 three-character country code, if not found set to --
   # X_GEOIP_COUNTRY - The ISO3166 English-language name of the country, if not found set to N/A
   # X_GEOIP_CONTINENT - The two-character continent code, if not found set to --
+  # X_GEOIP_REGION - The English-language name of the region, if not found set to "" (e.g. "CA")
+  # X_GEOIP_CITY - The English-language name of the city, if not found set to "" (e.g. "Mountain View")
+  # X_GEOIP_POSTAL_CODE - The postal code, if not found set to "" (e.g. "94043")
+
   #
   #
   # You can use the included Mapping class to trigger lookup only for certain requests by specifying matching path prefix in options, eg:
@@ -54,6 +58,9 @@ module Rack
         env['X_GEOIP_COUNTRY_CODE3'] = res['country_code3']
         env['X_GEOIP_COUNTRY'] = res['country_name']
         env['X_GEOIP_CONTINENT'] = res['continent_code']
+        env['X_GEOIP_REGION'] = res['region_name']
+        env['X_GEOIP_CITY'] = res['city_name']
+        env['X_GEOIP_POSTAL_CODE'] = res['postal_code']
       else
         env['X_GEOIP_MATCHED'] = 0
       end
